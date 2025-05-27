d80110501b981b20f6d56006be6d1ddfd9ffffbf
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-05-27 15:46:33 -0400

    MVP home

---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+import chespin from "./../assets/chespin.png";
+
+const Hero = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={chespin} className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">¡Bienvenido, Entrenador!</h1>
+          <p className="py-6">
+            ¿Listo para la aventura definitiva? Con esta app, finalmente podrás
+            llevar un registro perfecto de tu Master Set Pokémon. Olvídate de
+            las listas interminables y los "lo tengo o no lo tengo" en el aire.
+            Aquí, cada carta de tu colección tiene su lugar.
+          </p>
+          <button className="btn btn-primary">Empezar Ahora!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Home from "./pages/Home/index.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <App />
+      <Home />
     </QueryClientProvider>
   </StrictMode>
 );
