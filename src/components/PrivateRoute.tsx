704d23216a9b89f8897a18dcd349927c3aa6ce33
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-05-27 18:24:31 -0400

    refactor: improve loading state display in PrivateRoute component

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -9,26 +9,18 @@ interface PrivateRouteProps {
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const { currentUser, loading } = useAuth();
 
-  // 1. Manejar el estado de carga inicial
-  // Mientras Firebase está verificando si hay un usuario autenticado,
-  // mostramos un mensaje de carga para evitar un "parpadeo" o redirección incorrecta.
   if (loading) {
-    return <div>Cargando autenticación...</div>; // Puedes reemplazar esto con un spinner o un componente de carga más elegante.
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        <span className="loading loading-ring loading-xl"></span>
+      </div>
+    );
   }
 
-  // 2. Verificar si hay un usuario autenticado
-  // Si 'loading' es falso y 'currentUser' es null, significa que no hay usuario logeado.
-  // En este caso, redirigimos al usuario a la página de login.
   if (!currentUser) {
-    // <Navigate to="/login" replace /> redirige a la URL especificada.
-    // 'replace' evita que la página protegida se agregue al historial del navegador.
-    // Así, si el usuario presiona "atrás" en el navegador, no volverá a la página protegida sin autenticarse.
     return <Navigate to="/welcome" replace />;
   }
 
-  // 3. Si el usuario está autenticado, renderiza el contenido de la ruta protegida
-  // Si llegamos a este punto, significa que 'loading' es false y 'currentUser' no es null.
-  // Por lo tanto, el usuario está autenticado y puede ver el contenido de la ruta.
   return <>{children}</>;
 };
 
