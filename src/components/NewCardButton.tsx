c324f40453827256aa7c22f3965b89beb81fa83a
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-05-27 18:41:54 -0400

    feat: add CardInCollection and NewCardButton components to Home page

---
+++ src/components/NewCardButton.tsx
@@ -0,0 +1,9 @@
+const NewCardButton = () => {
+  return (
+    <div className="card card-border bg-base-100 w-80 h-50 flex items-center justify-center">
+      +
+    </div>
+  );
+};
+
+export default NewCardButton;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import CardInCollection from "../../components/CardInCollection";
+import NewCardButton from "../../components/NewCardButton";
 import { useAuth } from "../../context/AuthContext";
 const Home = () => {
   const { currentUser, loading } = useAuth();
@@ -6,10 +8,16 @@ const Home = () => {
 
   return (
     <div>
-      <h1>
-        Home, acá va mi colección -{" "}
-        {loading ? "cargando" : currentUser?.displayName}
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Mi colección - {loading ? "cargando" : currentUser?.displayName}
       </h1>
+
+      <div className="container justify-center mx-auto mt-10 flex flex-wrap gap-4">
+        <NewCardButton />
+
+        <CardInCollection />
+        <CardInCollection />
+      </div>
     </div>
   );
 };
