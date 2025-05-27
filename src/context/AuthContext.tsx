a972cef0e1f43abbdeb201e7fdf1beb2e7ec5d1c
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-05-27 17:37:42 -0400

    feat: implement authentication context and protected route functionality

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+// src/context/AuthContext.tsx
+import React, { createContext, useContext, useEffect, useState } from "react";
+//@ts-expect-error function exists but no types
+import { User, onAuthStateChanged } from "firebase/auth";
+// @ts-expect-error function exists but no types
+import { auth } from "../config/firebase";
+
+interface AuthContextType {
+  currentUser: User | null;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
