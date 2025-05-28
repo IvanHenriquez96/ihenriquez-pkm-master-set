5baa6cc85d7e6e808049588f4fb0fc86eef38dbc
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-05-27 20:20:38 -0400

    Implement code changes to enhance functionality and improve performance

--- src/components/CardInCollection.tsx
+++ src/components/CardInCollection.tsx
@@ -1,12 +1,23 @@
-import type { PokemonCard } from "../interfaces/pokemon-card";
+import PokeCardImg from "../assets/pokecard.png";
 
-const CardInCollection = (data: PokemonCard) => {
+interface CardInCollectionProps {
+  number: number;
+  name: string;
+}
+
+const CardInCollection = ({ number, name }: CardInCollectionProps) => {
   return (
-    <div className="card card-border bg-base-100 w-80">
-      <img src={data.images.small} alt={data.id} />
+    <div className="card card-border bg-base-100 w-80  shadow-xl">
+      <img
+        src={PokeCardImg}
+        alt="Pokemon Card"
+        loading="lazy"
+        className="grayscale w-64 object-cover  mx-auto mt-5"
+      ></img>
+
       <div className="card-body">
-        <h2 className="card-title">
-          {data.nationalPokedexNumbers[0]}-{data.name}
+        <h2 className="card-title mx-auto">
+          {number}-{name}
         </h2>
       </div>
     </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,36 @@
-import { useQuery } from "@tanstack/react-query";
 import CardInCollection from "../../components/CardInCollection";
 import NewCardButton from "../../components/NewCardButton";
 import { useAuth } from "../../context/AuthContext";
-import getPokemonCards from "../../api/getPokemonCards";
-import type { PokemonCard } from "../../interfaces/pokemon-card";
+import { pokemonList } from "../../constants/pokemonList";
+import { useState } from "react";
 
 const Home = () => {
   const { currentUser, loading } = useAuth();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["getPokemonCards"],
-    queryFn: getPokemonCards,
-  });
+  //1.- need check in firestore if user has pokemon cards
 
-  console.log("data:", data);
+  //2.-later, fetch the user's pokemon cards from Firestore
+
+  //3.-later pass the fetched data to the pokemonList variable
+
+  const indexOfLastPokemon = currentPage * itemsPerPage;
+  const indexOfFirstPokemon = indexOfLastPokemon - itemsPerPage;
+  const currentPokemons = pokemonList.slice(
+    indexOfFirstPokemon,
+    indexOfLastPokemon
+  );
+
+  const totalPages = Math.ceil(pokemonList.length / itemsPerPage);
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
 
   return (
     <div>
@@ -24,12 +41,33 @@ const Home = () => {
       <div className="container justify-center mx-auto mt-10 flex flex-wrap gap-4">
         <NewCardButton />
 
-        {isLoading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-
-        {data?.map((card: PokemonCard) => (
-          <CardInCollection key={card.id} {...card} />
+        {currentPokemons.map((pokemon) => (
+          <CardInCollection
+            key={pokemon.id}
+            number={pokemon.id}
+            name={pokemon.name}
+          />
         ))}
+      </div>
+
+      <div className="flex justify-center mt-8 space-x-4 mb-10">
+        <button
+          onClick={goToPrevPage}
+          disabled={currentPage === 1}
+          className="btn btn-primary" // Clase btn de DaisyUI y btn-primary para el color
+        >
+          Anterior
+        </button>
+        <span className="text-lg font-medium">
+          Página {currentPage} de {totalPages}
+        </span>
+        <button
+          onClick={goToNextPage}
+          disabled={currentPage === totalPages}
+          className="btn btn-primary" // Clase btn de DaisyUI y btn-primary para el color
+        >
+          Siguiente
+        </button>
       </div>
     </div>
   );
